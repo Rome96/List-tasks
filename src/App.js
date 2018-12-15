ef0ef14865665e7f5e1270826f2910bde646e0c0
Turiano Romero <romero.guzman18@gmail.com>
2018-12-14 22:19:27 -0500

    creación del proyecto

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          
           <a
             className="App-link"
             href="https://reactjs.org"
